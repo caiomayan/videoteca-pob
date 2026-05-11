3b6f588d6fc8952523a7cc2dda20e1650a52b4e7
MessiasTeixeira <messias4910@gmail.com>
2026-05-11 12:03:11 -0300

    feat: adiciona funcionalidade listar

--- src/appconsole/Listar.java
+++ src/appconsole/Listar.java
@@ -1,43 +1,46 @@
 package appconsole;
 
-import com.db4o.ObjectContainer;
-import com.db4o.query.Query;
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import modelo.Genero;
 import modelo.Video;
 import util.Util;
 
-import java.util.List;
-
 public class Listar {
-    private ObjectContainer manager;
+    private EntityManager manager;
 
-    public Listar() {
-        Util.conectar();
-
-        manager = Util.getManager();
-
-        System.out.println("__________ Lista de gêneros __________ ");
-        Query q = manager.query();
-        q.constrain(Genero.class);
-        List<Genero> listaGeneros = q.execute();
-        if(listaGeneros.isEmpty()) {
-            System.out.println("Nenhum gênero está cadastrado.");
-        }
-        for (Genero genero : listaGeneros) {
-            System.out.println(genero);
-        }
-
-        System.out.println("\n__________ Lista de vídeos __________ ");
-        q = manager.query();
-        q.constrain(Video.class);
-        List<Video> listaVideos = q.execute();
-        if (listaVideos.isEmpty()) {
-            System.out.println("Nenhum vídeo está cadastrado.");
-        }
-        for (Video video : listaVideos) {
-            System.out.println(video);
-        }
+    public Listar() { 
+    	try {
+	        Util.conectar();
+	
+	        manager = Util.getManager();
+	
+	        System.out.println("__________ Lista de gêneros __________ ");
+			TypedQuery<Genero> queryGenero = manager.createQuery("select g from Genero g", Genero.class); 
+			List<Genero> listaGeneros = queryGenero.getResultList();
+	        if(listaGeneros.isEmpty()) {
+	            System.out.println("Nenhum gênero está cadastrado.");
+	        }
+	        for (Genero genero : listaGeneros) {
+	            System.out.println(genero);
+	        }
+	
+	        System.out.println("\n__________ Lista de vídeos __________ ");
+			TypedQuery<Video> queryVideo = manager.createQuery("select v from Video v", Video.class); 
+			List<Video> listaVideos = queryVideo.getResultList();
+	        if (listaVideos.isEmpty()) {
+	            System.out.println("Nenhum vídeo está cadastrado.");
+	        }
+	        for (Video video : listaVideos) {
+	            System.out.println(video);
+	        }
+	    } catch (Exception e) {
+	    	System.out.println(e.getMessage());
+	    }
         Util.desconectar();
+
     }
     
 
